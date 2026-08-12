2c4ae94de19a6b7c185fada20daf1edbd0d06f2d
quaggosxr <arthur.quaggos200@gmail.com>
2026-08-12 08:26:33 -0300

    Delete dom directory

--- dom/ex04/script.js
+++
@@ -1,9 +0,0 @@
-const meuDivElemento = document.getElementById("meuDiv");
-
-meuDivElemento.addEventListener("mouseover", function() {
-    meuDivElemento.style.background = "lightgreen";
-});
-    
-meuDivElemento.addEventListener("mouseout", function(){
-    meuDivElemento.style.background = "lightblue"
-});
